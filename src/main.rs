f2ab942044f18d1d962fc94eb2896759f91c9ef3
Casper <casper141@gmail.com>
2025-10-22 20:24:41 +0300

    Tray icon somewhat working

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-
 mod app;
 mod collectors;
 
-use std::sync::mpsc;
+use std::process::exit;
+use std::sync::{mpsc, Mutex};
 use std::thread;
 use std::time::Duration;
 use app::ui_main_window::MainWindow;
 use eframe::egui::{self, Context, Visuals};
 use crate::collectors::cpu_collector::CpuData;
 use sysinfo::System;
-
-fn main() -> eframe::Result {
+use tray_icon::{menu::{Menu, MenuItem, MenuEvent}, Icon, MouseButtonState, TrayIconBuilder, TrayIconEvent};
+use windows::Win32::Foundation::HWND;
+use windows::Win32::UI::WindowsAndMessaging::{ShowWindow, SW_HIDE, SW_SHOWDEFAULT};
+use winit::raw_window_handle::{HasWindowHandle, RawWindowHandle};
+static VISIBLE: Mutex<bool> = Mutex::new(true);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    // ----TRAY ICON CRAP -----
+    let quit_i = MenuItem::with_id( "quit", "Quit", true, None);
+    let icon = Icon::from_path("assets/favicon.ico", None)?;
+    let tray_menu = Menu::new();
+    tray_menu.append_items(&[&quit_i])?;
+    let _tray_icon = TrayIconBuilder::new()
+        .with_menu(Box::new(tray_menu))
+        .with_icon(icon)
+        .with_tooltip("TempMon")
+        .build()?;
+    // ^^^^^ TRAY ICON CRAP ^^^^
+
+    // ----NATIVE WINDOW SETUP -----
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([520.0, 340.0])
@@ -29,12 +47,49 @@ fn main() -> eframe::Result {
         "TempMon",
         options,
         Box::new(|cc| {
+            let ctx = cc.egui_ctx.clone();
             set_styles(&cc.egui_ctx);
+
+            // TRAY ICON https://github.com/emilk/egui/discussions/737
+            let RawWindowHandle::Win32(handle) = cc.window_handle().unwrap().as_raw() else {
+                panic!("Unsupported platform");
+            };
+            MenuEvent::set_event_handler(Some(move |event: MenuEvent| {
+                if event.id == "quit" {
+                    exit(0);
+                }
+            }));
+            TrayIconEvent::set_event_handler(Some(move |event: TrayIconEvent| {
+
+                match event {
+                    TrayIconEvent::Click {
+                        button_state: MouseButtonState::Down,
+                        ..
+                    } => {
+                        let mut visible = VISIBLE.lock().unwrap();
+
+                        if *visible {
+                            let window_handle = HWND(handle.hwnd.into());
+                            unsafe {
+                                ShowWindow(window_handle, SW_HIDE);
+                            }
+                            *visible = false;
+                        } else {
+                            let window_handle = HWND(handle.hwnd.into());
+                            unsafe {
+                                ShowWindow(window_handle, SW_SHOWDEFAULT);
+                            }
+                            *visible = true;
+                        }
+
+                    }
+                    _ => (),
+                }
+            }));
+
             // THREAD TO UPDATE CPU DATA
             let (tx, rx) = mpsc::channel();
             let mut sys = System::new_all();
-            let ctx = cc.egui_ctx.clone();
-
             thread::spawn(move || {
                 loop {
                     sys.refresh_cpu_all();
@@ -48,8 +103,11 @@ fn main() -> eframe::Result {
 
             Ok(Box::new(MainWindow::new(rx)))
         }),
-    )
+    )?;
+
+    Ok(())
 }
+
 fn set_styles(ctx: &Context) {
     let mut style = (*ctx.style()).clone();
     style.visuals = Visuals::dark();
