f477ab9ec5f4acd10a44c58c77aca894c61f088f
Casper <casper141@gmail.com>
2025-10-26 00:03:15 +0300

    Testing scaling the app - iced-rs 0.14-dev

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use iced::widget::{button, column, container, pick_list, text};
-use iced::{window, Center, Element, Task, Theme};
+mod app;
+mod collectors;
+
+use iced::widget::{button, center, column, container, pick_list, row, text};
+use iced::window::Position::Centered;
+use iced::{window, Background, Center, Element, Fill, Task, Theme};
+use app::main_window;
 
 fn main() -> iced::Result {
     iced::daemon(App::new, App::update, App::view)
@@ -9,25 +14,47 @@ fn main() -> iced::Result {
         .run()
 }
 
-struct App {
-    window_id: Option<window::Id>,
-    current_theme: Theme,  // Store selected theme
-}
-
 #[derive(Clone, Debug)]
 enum Message {
     WindowOpened(window::Id),
     WindowClosed(window::Id),
-    ThemeChanged(Theme),  // New message
+    ThemeChanged(Theme),
+    MainButtonPressed,
+    PlotterButtonPressed,
+    SettingsButtonPressed,
+}
+#[derive(Clone, Debug)]
+enum Screen {
+    Main,
+    Plotter,
+    Settings,
 }
 
+struct App {
+    window_id: Option<window::Id>,
+    current_screen: Screen,
+    app_screen: Screen,
+    current_theme: Theme,
+}
 impl App {
     fn new() -> (Self, Task<Message>) {
-        let (_, open_task) = window::open(window::Settings::default());
+        let window_settings = window::Settings {
+            size: iced::Size::new(800.0, 600.0),
+            position: window::Position::Centered,
+            min_size: Some(iced::Size::new(500.0, 400.0)),
+            resizable: true,
+            decorations: true,
+            level: window::Level::Normal,
+            ..Default::default()
+        };
+
+        let (_, open_task) = window::open(window_settings);
 
         (
             Self {
                 window_id: None,
+                current_screen: Screen::Main,
+                app_screen: Screen::Main,
                 current_theme: Theme::GruvboxDark,
             },
             open_task.map(Message::WindowOpened),
@@ -49,7 +76,13 @@ impl App {
                 Task::none()
             }
             Message::ThemeChanged(theme) => {
-                self.current_theme = theme;  // Update theme
+                self.current_theme = theme;
+                Task::none()
+            }
+            Message::MainButtonPressed
+            | Message::PlotterButtonPressed
+            | Message::SettingsButtonPressed => {
+                println!("Button pressed");
                 Task::none()
             }
         }
@@ -59,28 +92,60 @@ impl App {
         if self.window_id != Some(window_id) {
             return container("").into();
         }
+        match self.current_screen {
+            Screen::Main => main_window::view(&self.current_theme),
+            Screen::Plotter => container("").into(),
+            Screen::Settings => container("").into(),
+        }
 
-        // Theme picker dropdown
-        let theme_picker = pick_list(
-            Theme::ALL,
-            Some(&self.current_theme),
-            Message::ThemeChanged,
-        )
-            .placeholder("Choose theme");
 
-        let content = column![
-            text("Pick a Theme:").size(20),
-            theme_picker,
-            text(""),
-        ]
-            .spacing(20)
-            .align_x(Center);
-
-        container(content)
-            .padding(40)
-            .center_x(iced::Fill)
-            .center_y(iced::Fill)
-            .into()
+        // let main_page_button = button("Main")
+        //     .on_press(Message::MainButtonPressed)
+        //     .style(|_theme: &Theme, status| button::primary(&Theme::Dark, status));
+        // let plotter_page = button("Plotter")
+        //     .on_press(Message::PlotterButtonPressed)
+        //     .style(|_theme: &Theme, status| button::primary(&Theme::Dark, status));
+        // let settings_page = button("Settings")
+        //     .on_press(Message::SettingsButtonPressed)
+        //     .style(|_theme: &Theme, status| button::primary(&Theme::Dark, status));
+        //
+        // let theme_picker = pick_list(Theme::ALL, Some(&self.current_theme), Message::ThemeChanged)
+        //     .placeholder("Choose theme")
+        //     .text_size(15)
+        //     .width(150);
+        //
+        // let theme_section = container(
+        //     row![main_page_button, plotter_page, settings_page, theme_picker]
+        //         .align_y(Center)
+        //         .spacing(10),
+        // )
+        //     .align_x(Center)
+        //     .align_y(Center)
+        //     .style(|theme: &Theme| {
+        //         let dracula_bg = Theme::Nord.extended_palette().background.base.color;
+        //
+        //         container::Style {
+        //             background: Some(Background::Color(dracula_bg)),
+        //             border: iced::Border {
+        //                 color: iced::Color::from_rgba(0.1, 0.5, 0.5, 0.0),
+        //                 width: 1.0,
+        //                 radius: iced::border::Radius {
+        //                     top_left: 0.0,
+        //                     top_right: 0.0,
+        //                     bottom_left: 10.0,
+        //                     bottom_right: 10.0,
+        //                 },
+        //             },
+        //             ..Default::default()
+        //         }
+        //     })
+        //     .width(450)
+        //     .height(50);
+        //
+        // container(theme_section)
+        //     .width(Fill)
+        //     .center_x(Fill)
+        //     .into()
     }
 
     fn subscription(&self) -> iced::Subscription<Message> {
